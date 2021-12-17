eeec0e7c6931218066fbe6fb9ce39583953698b9
WaltRG <95108974+WaltRG@users.noreply.github.com>
2021-12-17 17:36:55 -0600

    si

--- ProyectoClienteServidor/src/proyectoclienteservidor/Main.java
+++ ProyectoClienteServidor/src/proyectoclienteservidor/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package proyectoclienteservidor;
 public class Main {
     public static void main(String[] args) {
@@ -10,3 +11,41 @@ public class Main {
     }
     
 }
+=======
+package proyectoclienteservidor;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+        v.setLocationRelativeTo(null);
+        v.setTitle("xd");
+
+//agregaPlato agreg=new agregaPlato();
+//agreg.setVisible(true);
+//agreg.setLocationRelativeTo(null);
+//agreg.setTitle("Agregar Plato");
+
+//eliminaPlato eli=new eliminaPlato();
+//
+//eli.setVisible(true);
+//eli.setLocationRelativeTo(null);
+//eli.setTitle("Eliminar Plato");
+
+//String[] options = {"OK"};
+//
+//
+//
+//JOptionPane.showInputDialog(null, "Examen Tijuanas PR",null,
+//     JOptionPane.QUESTION_MESSAGE, null, options[0],options );
+
+
+
+        
+
+    }
+    
+}
+>>>>>>> Stashed changes
