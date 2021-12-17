5ee4523373bdc0c4e8fa912551f24f56265fa7b1
Gabrielmong <84334533+Gabrielmong@users.noreply.github.com>
2021-12-17 17:41:04 -0600

    Final

--- ProyectoClienteServidor/src/proyectoclienteservidor/Main.java
+++ ProyectoClienteServidor/src/proyectoclienteservidor/Main.java
@@ -1,51 +1,14 @@
-<<<<<<< Updated upstream
 package proyectoclienteservidor;
-public class Main {
-    public static void main(String[] args) {
-        Administrador v = new Administrador();
-        v.setVisible(true);
-        v.setLocationRelativeTo(null);
-        v.setTitle("Login");
-        //11
-
-    }
-    
-}
-=======
-package proyectoclienteservidor;
-
-import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
         Ventana v = new Ventana();
         v.setVisible(true);
         v.setLocationRelativeTo(null);
-        v.setTitle("xd");
-
-//agregaPlato agreg=new agregaPlato();
-//agreg.setVisible(true);
-//agreg.setLocationRelativeTo(null);
-//agreg.setTitle("Agregar Plato");
-
-//eliminaPlato eli=new eliminaPlato();
-//
-//eli.setVisible(true);
-//eli.setLocationRelativeTo(null);
-//eli.setTitle("Eliminar Plato");
-
-//String[] options = {"OK"};
-//
-//
-//
-//JOptionPane.showInputDialog(null, "Examen Tijuanas PR",null,
-//     JOptionPane.QUESTION_MESSAGE, null, options[0],options );
-
-
-
-        
+        v.setTitle("Main Menu");
+    
 
     }
     
 }
->>>>>>> Stashed changes
+
